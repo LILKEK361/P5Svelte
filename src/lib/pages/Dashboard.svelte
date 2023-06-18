1b571de4c2031fce1659b928ca2972c809a01c40
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-06-18 17:04:55 +0200

    Changes by nickh - creating table

--- src/lib/pages/Dashboard.svelte
+++ src/lib/pages/Dashboard.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 
+    import DataTable from "$lib/uicomponents/pagecomponents/DataTable.svelte";
+    import LineChart from "$lib/uicomponents/charts/LineChart.svelte";
 </script>
 
 <div>
     <h1>Dashboard</h1>
+
+    <!-- Table -->
+
+    <DataTable></DataTable>
+
+    <!--Doughnut Chart -->
+
+    <LineChart/>
+
 </div>
