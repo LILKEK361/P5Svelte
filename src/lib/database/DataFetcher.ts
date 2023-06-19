8ccdcd9b9d0404eb830083fae96900cfb64b4ccb
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-19 07:28:34 +0200

    added a function to get singledata

--- src/lib/database/DataFetcher.ts
+++ src/lib/database/DataFetcher.ts
@@ -1,4 +1,4 @@
-import {child, get, ref} from "firebase/database";
+import {child, get, ref, onValue} from "firebase/database";
 import {db} from "$lib/database/DatabaseConfig";
 
 export function fetchCo2Data(): Array<number> {
@@ -10,4 +10,13 @@ export function fetchCo2Data(): Array<number> {
         console.error("Failed to get Co2 overtime from db. Error: " + err);
     });
     return [];
+}
+
+export async function getsingeldata(path : string){
+    const startref = ref(db, path)
+    let singledata;
+    await onValue(startref, (snapshot) => {
+        singledata = snapshot.val()
+    })
+    return singledata;
 }
