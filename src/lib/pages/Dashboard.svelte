9ae62edb89d87512961e2837fdb5bfe3cf639166
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-08-26 11:45:13 +0200

    Created doughnut chart

--- src/lib/pages/Dashboard.svelte
+++ src/lib/pages/Dashboard.svelte
@@ -3,6 +3,7 @@
     import LineChart from "$lib/uicomponents/charts/LineChart.svelte";
     import {checkValueForTable} from "$lib/assets/TypeScriptCode/extracode";
     import {getSpecificData, MinMaxValuesforElements, pathsForElements} from "$lib/database/DatabaseConfig";
+    import DonutChart from "$lib/uicomponents/charts/DonutChart.svelte";
 
     const allElements = ["AirQuality", "CO2", "N2", "O2"]
     const allValuesForElements = {"AirQuality" : 0, "CO2" : 0, "N2" : 0, "O2" : 0}
@@ -18,8 +19,8 @@
 
 </script>
 
-<div>
-    <h1>Dashboard</h1>
+<div class="overflow-hidden flex items-center">
+
 
     <!-- Table -->
     <table class="border-2 border-white max-w-[50vw] min-w-[40vw] min-h-[20vh]  max-h-[50vh] p-0 m-0 ">
@@ -39,7 +40,7 @@
 
     <!--Doughnut Chart -->
 
-    <LineChart/>
+    <DonutChart />
 
 </div>
 
---
+++ src/lib/uicomponents/charts/DonutChart.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+    import {Chart} from 'chart.js/auto';
+
+
+    let chart : any;
+
+    const data = {
+        labels: ['5', '4', '3', '2', '1', '0'],
+        datasets: [{
+            label: 'Co2 over time',
+            data: [1, 2, 3, 1, 5, 6],
+            fill: true,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: .3
+        }]
+    };
+
+    const config = {
+        type: 'doughnut',
+        data: data,
+    };
+
+    onMount(() => {
+        const ctx = chart.getContext('2d');
+        var myChart = new Chart(ctx, config);
+    })
+
+
+
+</script>
+<div class="w-5/6 h-5/6">
+    <canvas bind:this={chart} class="h-full w-full" > </canvas>
+</div>
